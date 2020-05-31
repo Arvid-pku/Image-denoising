d569712f9db83e06a8ae8a94a1bd20de6473b882
ArvidYin <xjyin2019@163.com>
2020-05-31 15:07:59 +0800

    rebuild the already known space

---
+++ reKnown.py
@@ -0,0 +1,7 @@
+def reKnow(orgp, denoised):
+    [r, c, s] = orgp.shape()
+    for i in range(0, r):
+        for j in range(0, c):
+            if orgp[i, j, 0] + orgp[i,j, 1] + orgp[i,j, 2] != 0:
+                for k in range(0, s):
+                    denoised[i,j, k]=orgp[i,j,k]
